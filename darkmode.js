b5ac3f698cce5f9c4d966d7538172219c10ad36c
MonkejTV <johnsito.morales@outlook.com>
2024-02-01 11:32:09 +0100

    Update v.10

--- darkmode.js
+++ darkmode.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', function () {
     // Kolla om mörkläggningsläget är aktivt
     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
         // Mörkt läge är aktivt, genomför ändringar på din webbplats
-        document.body.style.backgroundColor = '#000';
-        document.body.style.color = '#F8F8FF';
+        document.body.classList.add('dark-mode');
     }
 });
