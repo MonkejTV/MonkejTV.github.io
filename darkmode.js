d546f2b077825e3d1a2a841095dae12c4850b8dd
MonkejTV <johnsito.morales@outlook.com>
2024-02-01 12:36:25 +0100

    Update v.11

--- darkmode.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Kolla om mörkläggningsläget är aktivt
-    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        // Mörkt läge är aktivt, genomför ändringar på din webbplats
-        document.body.classList.add('samsung-dark-mode');
-    }
-});
