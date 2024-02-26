134574a02a2d274ce09805c1ec026db27f123971
MonkejTV <johnsito.morales@outlook.com>
2024-02-26 18:42:10 +0100

    new stuff

--- homelink.js
+++ homelink.js
@@ -1,7 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Lägg till klickhändelse för "home" länken
-    var homeLink = document.getElementById('home-link');
-    homeLink.addEventListener('click', function (event) {
+    // Lägg till klickhändelse för "home" länken i logon
+    var logoLink = document.getElementById('logo-link');
+    logoLink.addEventListener('click', function (event) {
+        event.preventDefault(); // Förhindra standardbeteendet för länken
+
+        // Uppdatera URL utan att ladda om sidan
+        history.pushState({}, document.title, window.location.origin);
+
+        // Nu kan du göra de andra ändringarna som du vill göra när du går till framsidan
+    });
+
+    // Lägg till klickhändelse för "home" länken i navigationsmenyn
+    var navHomeLink = document.getElementById('nav-home-link');
+    navHomeLink.addEventListener('click', function (event) {
         event.preventDefault(); // Förhindra standardbeteendet för länken
 
         // Uppdatera URL utan att ladda om sidan
