9525b670f5fac325c5611513b45d1a62778c8892
MonkejTV <johnsito.morales@outlook.com>
2024-03-01 12:04:16 +0100

    change

--- homelink.js
+++ homelink.js
@@ -20,4 +20,15 @@ document.addEventListener('DOMContentLoaded', function () {
 
         // Nu kan du göra de andra ändringarna som du vill göra när du går till framsidan
     });
+
+        // Lägg till klickhändelse för "home" länken i navigationsmenyn
+        var navHomeLink = document.getElementById('nav-project-link');
+        navHomeLink.addEventListener('click', function (event) {
+            event.preventDefault(); // Förhindra standardbeteendet för länken
+    
+            // Uppdatera URL utan att ladda om sidan
+            history.pushState({}, document.title, window.location.origin);
+    
+            // Nu kan du göra de andra ändringarna som du vill göra när du går till framsidan
+        });
 });
